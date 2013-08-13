3f72bd642c98bfab78bc36145f46f21d96fdf15c
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-13 16:51:55 +0200

    Added a class for error and exception message strings

---
+++ lib/SyntaxError.php
@@ -0,0 +1,20 @@
+<?php
+/******************************************************************************
+ *      SyntaxError.php                                                       *
+ *      - Contains all the different OFSLib error/exception messages          *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in July 2013                                                  *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+
+  class SyntaxError{
+
+  }
+?>
+
