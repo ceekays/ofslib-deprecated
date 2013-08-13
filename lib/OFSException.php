19891c28c282950d66f83ef95e9192802a27b41c
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-13 15:00:20 +0200

    Addeds an OFSException construct

---
+++ lib/OFSException.php
@@ -0,0 +1,59 @@
+<?php
+/******************************************************************************
+ *      OFSException.php                                                      *
+ *      - Handles different OFS exceptions                                    *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in July 2013                                                  *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+
+  class OFSException extends Exception {
+
+    /**
+     * Holds the actual exception message
+     * @var string
+     */
+    static $message;
+
+    /**
+     * Holds the debug backtrace
+     * @var string
+     */
+    static $stacktrace;
+
+    /**
+     * Creates an OFSException 
+     *
+     * @param   string  $exception the actual exception message
+     * @param   string  $option An optional parameter that modifies the exception message
+     * @return  OFSException object
+     *
+     */
+    public function OFSException($exception, $option=null){
+      parent::__construct($exception);
+
+      static::$stacktrace = debug_backtrace();
+
+      $last = static::$stacktrace[count(static::$stacktrace) - 1];
+
+      $file = $last['file'];
+      $line = $last['line'];
+
+      static::$message  = "<b>OFSException:</b> ";
+      static::$message .= sprintf($this->getMessage(), $option);
+      static::$message .= " in ";
+      static::$message .= $file;
+      static::$message .= " on line ";
+      static::$message .= $line;
+
+      OFSException::callStack();
+    }
+  }
+?>
+
