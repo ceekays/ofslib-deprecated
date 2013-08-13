6ab9eeceda095d95989b6370557b2617970e57bd
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-13 18:56:39 +0200

    Added methods that facilitate sending of OFS message to remote server

--- lib/OFSConnector.php
+++ lib/OFSConnector.php
@@ -33,6 +33,18 @@
     private static $_web_service_url = null;
 
     /**
+     * Holds the raw OFS request text
+     * @var     string $_request
+     */
+    private $_request;
+
+    /**
+     * Holds the raw OFS response text
+     * @var     string $_response
+     */
+    private $_response;
+
+    /**
      * Connects to the remote T24/Globus server and keeps a persistent connection
      *
      * @param   string $web_service_url the actual webservice link
@@ -63,12 +75,97 @@
 
     /**
      * Sets the default OFS channel
-     * @param  string $ofs_source the name of the OFS channel that will be used
-     * @return
+     * @param  string $channel the name of the OFS channel that will be used
+     * @return void
      *
      */
     public static function set_default_channel($channel){
       self::$_channel['default'] = $channel;
+    }
+
+    /**
+     * Executes given OFS message
+     * @param  string $request the OFS request message
+     * @return object $this
+     *
+     */
+    protected function execute_ofs($request){
+      $channel_to_use   = null;
+      $this->_response  = null;
+
+      $this->_request = $request;
+
+      if($this->has_custom_channel())
+        $channel_to_use = $this->get_custom_channel();
+      else
+        $channel_to_use = $this->get_default_channel();
+
+      if(empty($channel_to_use))
+        throw new OFSException(SyntaxError::UNDEFINED_CHANNEL);
+
+      $this->send_ofs($channel_to_use);
+
+      return $this;
+    }
+
+    /**
+     * Checks whether the current OFS instance has a custom channel
+     * @return boolean $has_channel
+     *
+     */
+    protected function has_custom_channel(){
+      $has_channel = false;
+
+      if((isset(self::$_channel['custom']) && !empty(self::$_channel['custom']))
+        $has_channel = true;
+      else
+        $has_channel = false;
+
+      return $has_channel;
+    }
+
+    /**
+     * Returns a default OFS channel
+     * @returns self::$_channel['default']
+     */
+    protected function get_default_channel(){
+      return isset(self::$_channel['default']) ? self::$_channel['default'] : null;
+    }
+
+    /**
+     * Returns a custom OFS channel
+     * @returns self::$_channel['custom']
+     */
+    protected function get_custom_channel(){
+      return isset(self::$_channel['custom']) ? self::$_channel['custom'] : null;
+    }
+
+    /**
+     * Sends the OFS message to the remote T24/Globus server
+     * @returns void
+     */
+    private function send_ofs($channel_to_use){
+      $this->_response = null;
+
+      try{
+        if(empty($channel_to_use))
+          throw new OFSException(SyntaxError::UNDEFINED_CHANNEL);
+
+        if(empty(self::$_web_service_url))
+          throw new OFSException(SyntaxError::UNDEFINED_WEBSERVICE);
+
+        $this->_response = self::$_soap_client->executeOFS(
+          $channel_to_use,
+          $this->_request
+        );
+      }
+      catch(SoapFault $ex){
+        $this->_response = null;
+        throw new OFSException(
+          SyntaxError::SOAPFAULT_EXCEPTION,
+          $ex->faultstring
+        );
+      }
     }
   }
 ?>
