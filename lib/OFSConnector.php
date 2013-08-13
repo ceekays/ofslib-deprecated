a5a9ace7764f58de6ff61d9b770cc48ff265a8b1
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-13 17:04:02 +0200

    Added an interface that manages OFS connection settings for the remote server

---
+++ lib/OFSConnector.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *      OFSConnector.php                                                      *
+ *      - Manages OFS connection settings for the remote server               *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created on July 01, 2013                                              *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class OFSConnector{
+
+  }
+?>
