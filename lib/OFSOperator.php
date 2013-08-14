e9c2591f3d8d00b6ad18dda9e86698e731fb990b
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-14 18:50:53 +0200

    Added a class of OFS Operator definitions

---
+++ lib/OFSOperator.php
@@ -0,0 +1,17 @@
+<?php
+/******************************************************************************
+ *      OFSOperator.php                                                       *
+ *      - Contains OFS Operator definitions                                   *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+class OFSOperator {
+}
+?>
