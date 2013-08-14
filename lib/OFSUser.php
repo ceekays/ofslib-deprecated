078235642c5c0b1d009b3fa514b6264840cbe96c
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-14 12:43:28 +0200

    Added a class for OFS user information

---
+++ lib/OFSUser.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+ *      OFSUser.php                                                           *
+ *      - Defines an interface for OFS user information                       *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in July 2013                                                  *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+
+  class OFSUser{
+  }
+?>
+
