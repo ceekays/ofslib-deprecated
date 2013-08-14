72a8d1c61f6b04f7da8793eab1e07cafa69718d6
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-14 09:32:24 +0200

    Added a class for creating and executing an enquiry request

---
+++ lib/Enquiry/Request.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *      Request.php                                                           *
+ *      - Contains methods for creating and executing an enquiry request      *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Enquiry_Request extends OFSConnector {
+  }
+ ?>
+
