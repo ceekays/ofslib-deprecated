5a3811cb8fa1730e2ee4fa2a2f25c40141fbc4f0
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-14 16:32:58 +0200

    Added a method for adding a new enquiry data operation field

--- lib/Enquiry/Field.php
+++ lib/Enquiry/Field.php
@@ -13,6 +13,35 @@
  *                                                                            *
  ******************************************************************************/
   class Enquiry_Field {
+
+    /**
+     * Holds an enquiry field tuple
+     * @var $_fields
+     */
+    private $_fields = array();
+
+    /**
+     * Adds a new enquiry data operation field
+     * @param string      $field    the enquiry field name
+     * @param OFSOperator $operator the operator see OFSOperator.php for operator types
+     * @param string      $value    the data content
+     *  @returns object $this
+     *
+     */
+    public function add($field, $operator, $value){
+
+      $actual_operator = OFSOperator::get_operator_name($operator);
+
+      $data_content = array(
+        Enquiry::FIELD    => $field,
+        Enquiry::OPERATOR => $actual_operator,
+        Enquiry::VALUE    => $value
+      );
+
+      $this->_fields[] = $data_content;
+
+      return $this;
+    }
   }
 ?>
 
