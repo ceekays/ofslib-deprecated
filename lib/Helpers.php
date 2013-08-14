41a2052db5b130ce37552dd16d86ba80bbaba4c9
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-14 17:13:26 +0200

    Contains miscellaneous handy functions

---
+++ lib/Helpers.php
@@ -0,0 +1,17 @@
+<?php
+/******************************************************************************
+ *      Helpers.php                                                           *
+ *      - Contains miscellaneous handy functions                              *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+
+?>
+
