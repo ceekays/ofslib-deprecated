fa8bb9c4fb52bb5a97d0551fc49dee8ca009cfea
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-14 11:59:17 +0200

    Added an interface for manipulating enquiry fields

---
+++ lib/Enquiry/Field.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *      Field.php                                                             *
+ *      - Defines an interface for manipulating enquiry fields                *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Enquiry_Field {
+  }
+?>
+
