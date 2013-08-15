c83ce4b8a2678f037cb34c75c1d800cc3679278e
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-15 09:14:24 +0200

    Added a method that reads data from internal attributes

--- lib/Enquiry/Response.php
+++ lib/Enquiry/Response.php
@@ -13,6 +13,48 @@
  *                                                                            *
  ******************************************************************************/
   class Enquiry_Response extends OFSConnector {
+
+    /**
+     * Holds a list of fields that are forbidden from modifying
+     * @var $_forbidden_fields
+     */    
+    private static $_forbidden_fields = array('message', 'text');
+
+    /**
+     * Holds a list of accessible fields
+     * @var $_options_list
+     */
+    private static $_options_list = array('message', 'text');
+
+    /**
+     * Holds the raw response message
+     * @var $_response
+     */
+    protected $_response;
+
+    /**
+     * Reads data from internal attributes
+     *
+     * @param   string $option the name of the property
+     * @returns mixed  $value
+     *
+     */
+    public function __get($option){
+      $value = null;
+
+      if(!in_array($option, $this->_options_list))
+        throw new OFSException(SyntaxError::WRONG_DATA, $option);
+
+      switch($option){
+        case 'text':
+        case 'message':
+          $value = $this->_response;
+        break;
+      }
+
+      return $value;
+    }
+
   }
  ?>
 
