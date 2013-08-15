02efd307242bd404627ed01a5cea8cb9507c7633
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-15 18:18:48 +0200

    Added a class for creating and executing a transaction request

---
+++ lib/Transaction/Request.php
@@ -0,0 +1,17 @@
+<?php
+/******************************************************************************
+ *      Request.php                                                           *
+ *      - Contains methods for creating and executing a transaction request   *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Transaction_Request extends OFSConnector {
+  }
+?>
