690c60700e9f6f3697eb123512121347214020f4
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-15 08:29:43 +0200

    Added a class for manipulating an enquiry response

---
+++ lib/Enquiry/Response.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *      Response.php                                                          *
+ *      - Contains methods for manipulating an enquiry response               *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Enquiry_Response extends OFSConnector {
+  }
+ ?>
+
