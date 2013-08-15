5a07ff95ee468893bde1879e51340bd9e4e8e42c
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-15 18:36:09 +0200

    Added several significant variables

--- lib/Transaction/Request.php
+++ lib/Transaction/Request.php
@@ -13,5 +13,71 @@
  *                                                                            *
  ******************************************************************************/
   class Transaction_Request extends OFSConnector {
+
+    /**
+     * Holds the raw request message
+     * @var $_request
+     */
+    private $_request = null;
+
+    /**
+     * Holds a list of fields that are forbidden from modifying
+     * @var $_forbidden_fields
+     */
+    private static $_forbidden_fields = array(
+      'message',
+      'text'
+    );
+
+    /**
+     * Holds a list of accessible fields
+     * @var $_options_list
+     */
+    private static $_options_list = array(
+      'message',
+      'message_id',
+      'name',
+      'operation',
+      'text',
+      'transaction_id'
+    );
+
+    /**
+     * Holds transaction details: transaction_id and message_id
+     * @var $_transaction_details
+     */
+    private $_transaction_details = array();
+
+    /**
+     * Holds Transaction_Field object
+     * @var $fields
+     */
+    public $fields = null;
+
+    /**
+     * Holds the name of an transaction operation
+     * @var $_operation
+     */
+    public $operation = null;
+
+    /**
+     * Holds the name of an transaction operation
+     * @var $_operation
+     */
+    public $options = null;
+
+    /**
+     * Holds Transaction_Response object
+     * @var $response
+     */
+    public $response = null;
+
+    /**
+     * Holds OFSUser object
+     * @var $user
+     */
+    public $user = null;
+
   }
 ?>
+
