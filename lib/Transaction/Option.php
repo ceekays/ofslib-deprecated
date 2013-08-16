b3f573a2b2cab2baab61f769a9c58a4847c4a648
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 10:16:23 +0200

    Added an interface class for transaction options

---
+++ lib/Transaction/Option.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *      Option.php                                                            *
+ *      - Defines an interface for transaction options                        *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Transaction_Option{
+  }
+?>
+
