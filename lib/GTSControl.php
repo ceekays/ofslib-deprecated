54bd4d3423626743851a1fd5ff1dc8951ed9d64b
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 15:10:08 +0200

    Added for GTS Control settings

---
+++ lib/GTSControl.php
@@ -0,0 +1,21 @@
+<?php
+/******************************************************************************
+ *      GTSControl.php                                                        *
+ *      - Defines GTS Control settings                                        *
+ *                                                                            *
+ *      - GTS Control defines what should happen when                         *
+ *        an error or an override occurs during OFS processing                *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+
+  class GTSControl{}
+?>
+
