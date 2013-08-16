9bebe06a79b9bb47fe8adbab86df48725103b555
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 20:09:14 +0200

    Added an interface class for managing transaction fields

---
+++ lib/Transaction/Field.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *      Field.php                                                             *
+ *      - Defines an interface for managing transaction fields                *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Transaction_Field {
+  }
+?>
+
