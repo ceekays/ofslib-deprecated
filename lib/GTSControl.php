d10bca6fb5676ba3710554f6e677de2ca0f9f1de
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 15:57:26 +0200

    Added a list of allowed GTS Control Values

--- lib/GTSControl.php
+++ lib/GTSControl.php
@@ -16,6 +16,27 @@
  *                                                                            *
  ******************************************************************************/
 
-  class GTSControl{}
+  class GTSControl{
+
+    /**
+     * A list of allowed GTS Control Values
+     */
+    const HOLD_ALL_TRANSACTIONS                   = 'ba95e787-5a3b-4130-b406-41ee3e53f5b2';
+    const HOLD_ON_BOTH_ERROR_AND_OVERRIDE         = 'a9490ee4-d7a6-4feb-8b11-0289299a5bed';
+    const HOLD_ON_ERROR_AND_APPROVE_ON_OVERRIDE   = '5bd371fe-2dff-4bca-8883-529c808da536';
+    const REJECT_ON_ERROR_AND_APPROVE_ON_OVERRIDE = 'b86834d1-0a74-40fb-a0f9-ca58c448d132';
+    const REJECT_ON_ERROR_AND_HOLD_ON_OVERRIDE    = 'cb8392e7-7fc3-4d0d-b3c2-184129bc49ce';
+
+    /* alias for GTSControl::REJECT_ON_ERROR_AND_APPROVE_ON_OVERRIDE */
+    const USE_DEFAULT = GTSControl::REJECT_ON_ERROR_AND_APPROVE_ON_OVERRIDE;
+
+    private static $_gts_control_values = array(
+      GTSControl::REJECT_ON_ERROR_AND_APPROVE_ON_OVERRIDE  => '',
+      GTSControl::HOLD_ON_ERROR_AND_APPROVE_ON_OVERRIDE    => '1',
+      GTSControl::REJECT_ON_ERROR_AND_HOLD_ON_OVERRIDE     => '2',
+      GTSControl::HOLD_ON_BOTH_ERROR_AND_OVERRIDE          => '3',
+      GTSControl::HOLD_ALL_TRANSACTIONS                    => '4'
+    );
+  }
 ?>
 
