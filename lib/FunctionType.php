44c6b69d904047e7cb33edd5618a7f1162b5031d
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 12:56:35 +0200

    Added a list of allowed function types

--- lib/FunctionType.php
+++ lib/FunctionType.php
@@ -14,6 +14,17 @@
  ******************************************************************************/
 
   class FunctionType{
+
+    /**
+     * A list of allowed function types
+     */
+    const AUTHORISE = '666e16ab-f825-4b2c-887e-f43f721efb8b';
+    const DELETE    = '49970220-284e-4cda-8d2f-ac7efbe96de6';
+    const INPUT     = 'd9cef70b-bed9-4965-9ac6-55a2092dbc31';
+    const NONE      = 'e73e1555-2d26-4dc1-9186-445d2400cc2a';
+    const REVERSE   = '5eb22e74-7b58-4847-a798-b36061047f2a';
+    const SEE       = 'c158389b-fb48-41f4-af1b-01d5fb21f4b7';
+    const VERIFY    = 'c92b2962-b780-4c1a-87ea-0c09b13b16c6';
   }
 ?>
 
