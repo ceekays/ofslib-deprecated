3029f766780768c2470e2abd3f3c9c3cb0e934ed
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 16:59:01 +0200

    Added a class for controlling number of authorisers

---
+++ lib/Authoriser.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+ *      Authoriser.php                                                        *
+ *      - Definitions for controlling number of authorisers                   *
+ *                                                                            *
+ *                                                                            *
+ *      https://github.com/ceekays/ofslib                                     *
+ *                                                                            *
+ ******************************************************************************
+ *                                                                            *
+ *      Created in August 2013                                                *
+ *      by Edmond C. Kachale (Malawi)                                         *
+ *      (kachaleedmond [at] gmail [dot] com)                                  *
+ *                                                                            *
+ ******************************************************************************/
+  class Authoriser{
+  }
+?>
+
