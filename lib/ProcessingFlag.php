8b26c83d3da596881f675f6aa7bb4341e35f1e27
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-16 16:34:15 +0200

    Added a list of allowed processing flags

--- lib/ProcessingFlag.php
+++ lib/ProcessingFlag.php
@@ -14,5 +14,21 @@
  *                                                                            *
  ******************************************************************************/
   class ProcessingFlag{
+
+    /**
+     * A list of allowed processing flags
+     */
+    const PROCESS   = '0cebda77-312a-45b3-ae62-f7e57907902c';
+    const VALIDATE  = '5f5fc458-d24d-4fbf-97f7-6700725f59ee';
+
+    /**
+     * Holds the actual names of the processing flags
+     * @var $_processing_flag_names
+     */
+    private static $_processing_flag_names = array(
+      ProcessingFlag::PROCESS   => 'PROCESS',
+      ProcessingFlag::VALIDATE  => 'VALIDATE'
+    );
+
   }
 ?>
