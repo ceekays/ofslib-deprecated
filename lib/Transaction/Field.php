b415caf456cde5ec29cbc6dac6f51151fba4027a
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-19 09:01:36 +0200

    Modified to add a value field

--- lib/Transaction/Field.php
+++ lib/Transaction/Field.php
@@ -19,8 +19,9 @@
      *
      */
     const FIELD         = '83a2d051-9894-41f8-8cce-763145d351e1';
-    const MULTI_VALUE  = '686aaecf-998a-4414-9cb3-2e10ee5fe92f';
-    const SUB_VALUE    = '5e3e83e5-d882-4dd1-aa95-76b7ec952500';
+    const VALUE         = 'a6090045-d6c7-4e47-b78f-718db451785f';
+    const MULTI_VALUE   = '686aaecf-998a-4414-9cb3-2e10ee5fe92f';
+    const SUB_VALUE     = '5e3e83e5-d882-4dd1-aa95-76b7ec952500';
 
     /**
      * Holds a transaction field tuple
@@ -34,6 +35,7 @@
      */
     private static $_fieldset = array(
       Transaction_Field::FIELD        => 'field',
+      Transaction_Field::VALUE        => 'value',
       Transaction_Field::MULTI_VALUE  => 'multi_value',
       Transaction_Field::SUB_VALUE    => 'sub_value'
     );
@@ -47,10 +49,11 @@
      * @returns object $this
      *
      */
-    public function add($field, $multi_value=1, $sub_value=1){
+    public function add($field, $value, $multi_value=1, $sub_value=1){
 
       $data_content = array(
         self::$_fieldset[Transaction_Field::FIELD]        => $field,
+        self::$_fieldset[Transaction_Field::VALUE]        => $value,
         self::$_fieldset[Transaction_Field::MULTI_VALUE]  => $multi_value,
         self::$_fieldset[Transaction_Field::SUB_VALUE]    => $sub_value
       );
