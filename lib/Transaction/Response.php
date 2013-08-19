a53cf52a919b5ea64db65872b32c00d965a67ee7
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-19 17:01:41 +0200

    Added some variables for holding and manipulating a transaction response

--- lib/Transaction/Response.php
+++ lib/Transaction/Response.php
@@ -13,6 +13,24 @@
  *                                                                            *
  ******************************************************************************/
   class Transaction_Response extends OFSConnector {
+
+    /**
+     * Holds a list of fields that are forbidden from modifying
+     * @var $_forbidden_fields
+     */
+    private static $_forbidden_fields = array('message', 'text');
+
+    /**
+     * Holds a list of accessible fields
+     * @var $_options_list
+     */
+    private static $_options_list = array('message', 'text');
+
+    /**
+     * Holds the raw response message
+     * @var $_response
+     */
+    protected $_response;
   }
 ?>
 
