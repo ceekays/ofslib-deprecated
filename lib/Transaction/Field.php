7a2cfe96f160cd176d6813b4fa0039107faa37c4
Edmond Kachale <kachaleedmond@gmail.com>
2013-08-19 08:46:36 +0200

    Added a method for populating transaction fields

--- lib/Transaction/Field.php
+++ lib/Transaction/Field.php
@@ -18,9 +18,9 @@
      *   data operation constants for a transaction field
      *
      */
-    const FIELD               = '83a2d051-9894-41f8-8cce-763145d351e1';
-    const MULTI_VALUE_NUMBER  = '686aaecf-998a-4414-9cb3-2e10ee5fe92f';
-    const SUB_VALUE_NUMBER    = '5e3e83e5-d882-4dd1-aa95-76b7ec952500';
+    const FIELD         = '83a2d051-9894-41f8-8cce-763145d351e1';
+    const MULTI_VALUE  = '686aaecf-998a-4414-9cb3-2e10ee5fe92f';
+    const SUB_VALUE    = '5e3e83e5-d882-4dd1-aa95-76b7ec952500';
 
     /**
      * Holds a transaction field tuple
@@ -33,10 +33,32 @@
      * @var $_options_list
      */
     private static $_fieldset = array(
-      Transaction_Field::FIELD               => 'field',
-      Transaction_Field::MULTI_VALUE_NUMBER  => 'multi_value',
-      Transaction_Field::SUB_VALUE_NUMBER    => 'sub_value'
+      Transaction_Field::FIELD        => 'field',
+      Transaction_Field::MULTI_VALUE  => 'multi_value',
+      Transaction_Field::SUB_VALUE    => 'sub_value'
     );
+
+    /**
+     * Adds a new transaction field
+     *
+     * @param string  $field        the transaction field name
+     * @param int     $multi_value  multi value number
+     * @param int     $sub_value    sub-value number
+     * @returns object $this
+     *
+     */
+    public function add($field, $multi_value=1, $sub_value=1){
+
+      $data_content = array(
+        self::$_fieldset[Transaction_Field::FIELD]        => $field,
+        self::$_fieldset[Transaction_Field::MULTI_VALUE]  => $multi_value,
+        self::$_fieldset[Transaction_Field::SUB_VALUE]    => $sub_value
+      );
+
+      $this->_fields[] = $data_content;
+
+      return $this;
+    }
   }
 ?>
 
